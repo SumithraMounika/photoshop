a0ead836f0430ada6335c39b8544ba46370e9c59
User <user@Users-MacBook-Pro.local>
2020-03-30 15:28:40 +0800

    first

---
+++ server/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const cloudinary = require('cloudinary')
+const formData = require('express-form-data')
+const cors = require('cors')
+const { CLIENT_ORIGIN } = require('./config')
+
+const app = express()
+
+cloudinary.config({ 
+  cloud_name: 'ddid3ikmo', 
+  api_key: '272797986173728', 
+  api_secret: 'tjeabjv8Nwoqluhy4jK7RF-NwEM'
+})
+  
+app.use(cors({ 
+  origin: CLIENT_ORIGIN 
+})) 
+
+app.use(formData.parse())
+
+app.get('/wake-up', (req, res) => res.send('👌'))
+
+app.post('/image-upload', (req, res) => {
+
+  const values = Object.values(req.files)
+
+  const promises = values.map(image => cloudinary.uploader.upload(image.path))
+  
+  Promise
+    .all(promises)
+    .then(results => res.json(results))
+    .catch((err) => res.status(400).json(err))
+})
+
+app.listen(process.env.PORT || 8080, () => console.log('👍'))
